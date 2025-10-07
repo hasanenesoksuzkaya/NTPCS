bfe3d2e49d96afbb75ee2fb8d9fee0ba61ddca86
Hasan Enes Öksüzkaya <97636105+hasanenesoksuzkaya@users.noreply.github.com>
2025-10-07 08:54:36 +0300

    Update yedek.cs

--- yedek.cs
+++ yedek.cs
@@ -1,95 +1,183 @@
-/using System;
-
-//namespace Project1
-//{
-//    public class Program
-//    {
-//        static void Main()
-//        {
-//            Console.WriteLine("Ayın Kaçıncı Günü : ");
-//            int aGunu = int.Parse(Console.ReadLine());
-
-//            if (aGunu <= 0 || aGunu > 31)
-//            {
-//                Console.WriteLine("Lütfen 1 ile 31 arasında geçerli bir gün sayısı giriniz.");
-//                return;
-//            }
-
-//            int gun = aGunu % 7;
-//            if (gun == 0) gun = 7;
-
-//            switch (gun)
-//            {
-//                case 1:
-//                    Console.WriteLine("Pazartesi");
-//                    break;
-//                case 2:
-//                    Console.WriteLine("Salı");
-//                    break;
-//                case 3:
-//                    Console.WriteLine("Çarşamba");
-//                    break;
-//                case 4:
-//                    Console.WriteLine("Perşembe");
-//                    break;
-//                case 5:
-//                    Console.WriteLine("Cuma");
-//                    break;
-//                case 6:
-//                    Console.WriteLine("Cumartesi");
-//                    break;
-//                case 7:
-//                    Console.WriteLine("Pazar");
-//                    break;
-//                default:
-//                    Console.WriteLine("Ayın kaçındasın diye sormuştum.");
-//                    break;
-//            }
-//        }
-//    }
-//}
-
 using System;
-using System.Text;
 
-class Program
-//{
-//    static void Main()
-//    {
-//        Console.Write("Bir sayı girin: ");
-//        Int64 sayi = Convert.ToInt64(Console.ReadLine());
-
-//        Console.WriteLine($"1 ile {sayi} arasındaki asal sayılar:");
-
-//        StringBuilder asalSayilar = new StringBuilder();
-
-//        for (Int64 sayiAdim = 2; sayiAdim <= sayi; sayiAdim++)
-//        {
-//            if (AsalMi(sayiAdim))
-//            {
-//                if (asalSayilar.Length > 0)
-//                    asalSayilar.Append(", ");
-
-//                asalSayilar.Append(sayiAdim);
-//            }
-//        }
-
-//        Console.WriteLine(asalSayilar.ToString());
-//    }
-
-static bool AsalMi(Int64 sayi)
+public class Program
 {
-    if (sayi < 2)
-        return false;
-
-    for (Int64 i = 2; i * i <= sayi; i++)
+    static void Main()
     {
-        if (sayi % i == 0)
-            return false;
-    }
+        //double a, b,c ;
+        //Console.WriteLine("a kenarını giriniz: ");
+        //a = double.Parse (Console.ReadLine ());
 
-    return true;
-}
+        //Console.WriteLine("b kenarını giriniz: ");
+        //b = double.Parse(Console.ReadLine());
+
+        //c = Math.Sqrt((a * a) + (b * b));
+        //Console.WriteLine(c);
+        //Console.WriteLine($"Pisagorun sonucu: {c}");
+
+        //Console.WriteLine("1-7 arasında bir sayi giriniz: (1-Pazartesi, 2-Salı..)");
+        //int sayi = Convert.ToInt32(Console.ReadLine());
+
+        //switch (sayi)
+        //{
+        //    case 1:
+        //        Console.WriteLine("Pazartesi");
+        //        break;
+        //    case 2:
+        //        Console.WriteLine("Salı");
+        //        break;
+        //    case 3:
+        //        Console.WriteLine("çarşamba");
+        //        break;
+        //    case 4:
+        //        Console.WriteLine("perşembe");
+        //        break;
+        //    case 5:
+        //        Console.WriteLine(" bugün günlerden cuma");
+        //        break;
+        //    case 6:
+        //        Console.WriteLine("cumartesi");
+        //        break;
+        //    default: 
+        //        Console.WriteLine("pazar");
+        //        break;
+
+        /*
+        Console.Write("bir sayı giriniz: ");
+        int sayi = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"1 ile {sayi} arasındaki asal sayılar:");
+
+        for (int num = 2; num < sayi; num++)
+        {
+            bool asalMi = true;
+
+
+            for (int i = 2; i <= Math.Sqrt(num); i++)
+            {
+                if(num % i==0)
+                {
+                    asalMi = false;
+                    break;
+                }
+            }
+            if (asalMi)
+            {
+                Console.Write(num + "asal sayidir");
+            }
+            Console.ReadKey();
+        }
+        */
+
+        //Console.WriteLine("sayı gir: ");
+        //int a = Convert.ToInt32(Console.ReadLine());
+
+        //for(int i=2; i<=a; i++)
+        //{ 
+        //    for(int s=2; s<i; s++)
+        //    {
+
+        //        if(i%s==0)
+        //        {
+        //            break;
+        //        }
+        //        if(i%s !=0 && s==(i-1))
+        //        {
+        //            Console.WriteLine(i);
+        //        }
+        //    }
+        //}
+
+        //    Console.WriteLine("sayı gir: ");
+        //    int a = Convert.ToInt32(Console.ReadLine());
+        //    Console.WriteLine($" {a} sayısına kadar olan asal sayilar aşağıda listelenmiştir");
+
+        //for (int i = 2; i <= a; i++)
+        //{
+        //    if (AsalMi(i))
+        //        Console.WriteLine(i + "asal sayidir");
+        //}
+
+        //}
+        //static bool AsalMi(int sayi)
+        //{
+        //    if (sayi < 2) return false;
+        //    for(int i=2; i<= Math.Sqrt(sayi); i++)
+        //    {
+        //        if (sayi % i == 0)
+        //            return false;
+        //    }
+        //    return true;
+
+        //}
+
+        //        List<int> asalsayilar = new List<int>()
+        //        {
+        //            1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+        //        };
+        //        Console.WriteLine("girilen sayidan oluşan asal sayilar");
+        //       foreach(int asalsayi in asalsayilar)
+        //        {
+        //            for (int i = 2; i <= asalsayi; i++)
+        //            {
+        //                if (AsalMi(i))
+        //                    Console.WriteLine(i + "asal sayidir");
+        //            }
+
+        //        }
+
+
+        //    }
+        //    static bool AsalMi(int sayi)
+        //    {
+        //        if (sayi < 2) return false;
+        //        for (int i = 2; i <= Math.Sqrt(sayi); i++)
+        //        {
+        //            if (sayi % i == 0)
+        //                return false;
+        //        }
+        //        return true;
+
+        //    }
+        //}
+
+        /*while (true)
+        {
+            Console.WriteLine("Sayı Giriniz");
+            int sayı = Convert.ToInt32(Console.ReadLine());
+            if (sayı > 10)
+            {
+                Console.WriteLine("Sayı 10 dan Büyük Tebrikler");
+                break;
+            }
+            else {
+            Console.Clear();
+            Console.WriteLine("Sayı 10 dan Küçük Denemeye Devam Et"); }
+            
+            }
+        }
+    }*/
+
+        char secim;
+        do
+        {
+            Console.WriteLine("Bir sayi giriniz: ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            if (sayi < 10)
+            {
+                Console.WriteLine("Girilen sayı 10dan büyüktür");
+            }
+            else
+            {
+                Console.WriteLine("girilen sayı 10dan küçüktür");
+
+            }
+            Console.WriteLine("Tekrar Denemek ister misiniz? E/H");
+            secim = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+
+
+
+        } while (secim == 'E' || secim == 'e');
+        }
     }
-}
-}
